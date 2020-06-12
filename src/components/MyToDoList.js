ac6014b0a1717e27579a213d4953930f014dbc79
George Abraham <George.Abraham@incture.com>
2020-06-12 20:42:28 +0530

    comit

--- src/components/MyToDoList.js
+++ src/components/MyToDoList.js
@@ -9,10 +9,30 @@ class MyToDoList extends Component{
         this.state = {
             todos: todosData
         }
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
     }    
 
+    handleCheckboxChange(id) {        
+        this.setState(prevState => {
+            var newTodos = prevState.todos.map((prevTodo) => {
+                if (prevTodo.id == id) {
+                    // prevTodo.completed = !prevTodo.completed;
+                    return {
+                        ...prevTodo,
+                        completed: !prevTodo.completed
+                    }
+                }
+                return prevTodo;
+            })
+            return {
+                todos: newTodos
+            }
+        }) 
+        // alert("HI" + id);
+    }
+
     render() {
-        const todosCollection = this.state.todos.map(todoItems => <MyToDoListItem key={todoItems.id} content={todoItems}/>);
+        const todosCollection = this.state.todos.map(todoItems => <MyToDoListItem key={todoItems.id} content={todoItems} handleChange={this.handleCheckboxChange}/>);
         return (
             <div className="cltodo-list">
                 {todosCollection}
--- src/components/MyToDoListItem.js
+++ src/components/MyToDoListItem.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react"
-import "./MyToDoListItem.css"
+import React, { Component } from "react";
+import "./MyToDoListItem.css";
 
 class MyToDoListItem extends Component {
-    render() {
-        const styles = {};
-        styles.defaultChecked = this.props.content.completed;
-        styles.color = this.props.content.completed ? "green" : "blue";
-        styles.textDecoration = this.props.content.completed ? "line-through" : "none";
-        
-        return (
-            // <div className="cltodo-item">
-            //     <input type="checkbox" defaultChecked={props.content.completed ? true : false}/>
-            //     <p style={{color: props.content.completed ? "green" : "red", font}}>{props.content.text}</p>
-            // </div>
-            <div className="cltodo-item">
-                <input type="checkbox" checked={styles.defaultChecked} onChange={() => {console.log("Checkbox clicked!")}}/>
-                <p style={styles}>{this.props.content.text}</p>
-            </div>
-        )
-    }
+  render() {
+    const styles = {};
+    styles.defaultChecked = this.props.content.completed;
+    styles.color = this.props.content.completed ? "green" : "blue";
+    styles.textDecoration = this.props.content.completed
+      ? "line-through"
+      : "none";
+
+    return (
+      // <div className="cltodo-item">
+      //     <input type="checkbox" defaultChecked={props.content.completed ? true : false}/>
+      //     <p style={{color: props.content.completed ? "green" : "red", font}}>{props.content.text}</p>
+      // </div>
+      <div className="cltodo-item">
+        <input
+          type="checkbox"
+          checked={styles.defaultChecked}
+          onChange={(event) => this.props.handleChange(this.props.content.id)}
+        />
+        <p style={styles}>{this.props.content.text} </p>
+      </div>
+    );
+  }
 }
 
-export default MyToDoListItem
+export default MyToDoListItem;
